c98bf44c0b6345ba3cad87f56de5ee3d7e37a616
Hannah Joynson <hannahjoynson@gmail.com>
2022-04-27 13:51:23 +0100

    added second dice on javascript

--- javascript.js
+++ javascript.js
@@ -2,5 +2,25 @@
 //Generate random number from 1 to 6
 
 const firstRandomNum=
-Math.floor(Math.random() * 6) + 1
+Math.floor(Math.random() * 6) + 1 ;
+
+
+// images/dice1.jpg up to images/dice6.jpg
+
+const firstDiceImage = 'assets/dice' + firstRandomNum + '.png';
+
+document.querySelectorAll('img')[0].setAttribute('src', firstDiceImage);
+
+
+//Generate random number from 1 to 6
+
+const secondRandomNum=
+Math.floor(Math.random() * 6) + 1 ;
+
+
+// images/dice1.jpg up to images/dice6.jpg
+
+const secondDiceImage = 'assets/dice' + secondRandomNum + '.png';
+
+document.querySelectorAll('img')[1].setAttribute('src', secondDiceImage);
 
