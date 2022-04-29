2450fce02b17481c31cd574953ca121b470a5012
Hannah Joynson <hannahjoynson@gmail.com>
2022-04-29 14:39:59 +0100

    adding files for second player

--- javascript.js
+++ javascript.js
@@ -1,63 +1,19 @@
-// let User1Total = 0;
-
-// //Generate random number from 1 to 6
-
-// const firstRandomNum=
-// Math.floor(Math.random() * 6) + 1 ;
-
-
-// // images/dice1.jpg up to images/dice6.jpg
-
-// const firstDiceImage = 'assets/dice' + firstRandomNum + '.png';
-
-// document.querySelectorAll('img')[0].setAttribute('src', firstDiceImage);
-
-// //Generate random number from 1 to 6
-
-// const secondRandomNum=
-// Math.floor(Math.random() * 6) ;
-
-
-// // images/dice1.jpg up to images/dice6.jpg
-
-// const secondDiceImage = 'assets/dice' + secondRandomNum  + '.png';
-
-// document.querySelectorAll('img')[1].setAttribute('src', secondDiceImage);
-
-
-// // // Logic for winner
-
-// if (firstRandomNum > secondRandomNum) {
-//     document.querySelector('h1').innerHTML = 'The Winner is User 1';
-
-
-// } else if (firstRandomNum < secondRandomNum)
-// {
-//     document.querySelector('h1').innerHTML = 'The Winner is User 2';
-// } else { document.querySelector('h1').innerHTML = 'It is a draw';
-
-// }
-
-
-// //Button to refresh the page
-
-// const refreshButton = document.querySelector('.refresh-button');
-
-// const refreshPage = () => {
-//   location.reload();
-// }
-
-// refreshButton.addEventListener('click', refreshPage)
-
-
+//Player 1
 
 let User1Total = 0;
+
+let User2Total = 0;
+
+let player1turn = true;
+let player2turn = false;
+
 
 document.getElementById("roll").addEventListener("click", () => {  
     // Play a sound - slight delay - so maybe see if can cycle images /delay image or something
     // document.getElementById("sound").play()
 
-    // Generate a number
+    if (player1turn == true){
+        // Generate a number
     let diceRolled = Math.floor(Math.random()*6)+1;
 
 
@@ -66,44 +22,45 @@ document.getElementById("roll").addEventListener("click", () => {
 
     // Insert dice image
     document.querySelectorAll('img')[0].setAttribute('src', diceImage);
-    let score = User1Total += diceRolled;
-    document.getElementById("score").innerHTML = score
+    let score1 = User1Total += diceRolled;
+    document.getElementById("score1").innerHTML = score1
+
+    player1turn = false 
+    player2turn = true
+
+    } else if (player2turn == true){
+            // Generate a number
+    let diceRolled = Math.floor(Math.random()*6)+1;
 
 
-// Player 2
-// let User2Total = 1;
+    // Generate dice image 
+    const diceImage = 'assets/dice' + diceRolled + '.png';
 
-   
-//         // document.getElementById("sound").play()
-//     document.getElementById("roll").addEventListener("click", () => {  
-    
-    
-//         // Generate a number
-//         let diceRolled = Math.floor(Math.random()*6)+1;
-    
-    
-//         // Generate dice image 
-//         const seconddiceImage = 'assets/dice' + diceRolled + '.png';
-    
-//         // Insert dice image
-//         document.querySelectorAll('img')[1].setAttribute('src', diceImage);
-//         let score = User2Total += diceRolled;
-//         document.getElementById("score").innerHTML = score
+    // Insert dice image
+    document.querySelectorAll('img')[0].setAttribute('src', diceImage);
+    let score2 = User2Total += diceRolled;
+    document.getElementById("score2").innerHTML = score2 
 
+    player1turn = true
+    player2turn = false
 
-    // Create win/lose scenario 
-    if(score >=20){
-        document.getElementById("winLose").textContent = "You win!"
-        User1Total = 0;
     }
-    else if (diceRolled ==1){ 
-        document.getElementById("winLose").textContent = "You rolled 1! You lose!"
-        document.getElementById("score").textContent = ""
-        User1Total = 0;
-    }
+
+
+//Button to refresh the page
+
+const refreshButton = document.getElementById('refresh-button');
+
+const refreshPage = () => {
+  location.reload();
+}
+
+refreshButton.addEventListener('click',() => {
+document.getElementById("score1").textContent = 0 
+document.getElementById("score2").textContent = 0 
+document.getElementById("winLose").textContent = ""
+
+}) 
+
+
 })
-
-
-
-
-
